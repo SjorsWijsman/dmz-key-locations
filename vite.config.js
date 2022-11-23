3e936d5d381eb1331b8ab2942677b6eae794a128
Sjors Wijsman <sjorswijsman@gmail.com>
2022-11-23 23:17:35 +0100

    fixed github pages issues

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: "/dmz-key-locations/",
   build: {
     outDir: "./docs",
   },
