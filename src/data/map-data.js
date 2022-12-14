6a0d0e303ec3c4d7afba3561dd76d535b032453b
Faisal <f7000r@hotmail.com>
2022-12-14 21:45:44 +0300

    Update map-data.js

--- src/data/map-data.js
+++ src/data/map-data.js
@@ -167,33 +167,179 @@ export const deaddrops = [
 
 export const spawns = [
   {
-    title: "?",
+    title: "s1",
     location: {
       x: 3030,
       y: 2915
     }
   },
   {
-    title: "?",
+    title: "s2",
     location: {
       x: 2700,
       y: 3200,
     }
   },
   {
-    title: "?",
+    title: "s3",
     location: {
       x: 2132,
       y: 3376,
     }
   },
   {
-    title: "?",
+    title: "s4",
     location: {
       x: 1712,
       y: 3415,
     }
   },
-
-  
+  {
+    title: "s5",
+    location: {
+      x: 1456,
+      y: 3495,
+    }
+  },
+  {
+    title: "w1",
+    location: {
+      x: 1092,
+      y: 3088,
+    }
+  },
+  {
+    title: "w2",
+    location: {
+      x: 1078,
+      y: 2766,
+    }
+  },
+  {
+    title: "w3",
+    location: {
+      x: 718,
+      y: 2350,
+    }
+  },  {
+    title: "w4",
+    location: {
+      x: 942,
+      y: 2290,
+    }
+  },
+  {
+    title: "w5",
+    location: {
+      x: 716,
+      y: 1652,
+    }
+  },  {
+    title: "w7",
+    location: {
+      x: 1130,
+      y: 1748,
+    }
+  },
+  {
+    title: "w6",
+    location: {
+      x: 1124,
+      y: 1482,
+    }
+  },
+  {
+    title: "c1",
+    location: {
+      x: 2046,
+      y: 1700,
+    }
+  },
+  {
+    title: "c2",
+    location: {
+      x: 2004,
+      y: 1194,
+    }
+  },
+  {
+    title: "c3",
+    location: {
+      x: 1592,
+      y: 1742,
+    }
+  },
+  {
+    title: "c4",
+    location: {
+      x: 2315,
+      y: 1367,
+    }
+  },
+  {
+    title: "n1",
+    location: {
+      x: 1952,
+      y: 754,
+    }
+  },  {
+    title: "w8",
+    location: {
+      x: 1444,
+      y: 1212,
+    }
+  },
+  {
+    title: "n3",
+    location: {
+      x: 2350,
+      y: 958,
+    }
+  },
+  {
+    title: "n2",
+    location: {
+      x: 2160,
+      y: 782,
+    }
+  },  {
+    title: "e1",
+    location: {
+      x: 2722,
+      y: 1148,
+    }
+  },
+  {
+    title: "e2",
+    location: {
+      x: 2930,
+      y: 1518,
+    }
+  },  {
+    title: "e3",
+    location: {
+      x: 2930,
+      y: 1793,
+    }
+  },
+  {
+    title: "e4",
+    location: {
+      x: 2718,
+      y: 1980,
+    }
+  },  {
+    title: "e5",
+    location: {
+      x: 3264,
+      y: 2175,
+    }
+  },
+  {
+    title: "e6",
+    location: {
+      x: 3048,
+      y: 2495,
+    }
+  },
 ]
