dfe3c4ca0419d5feea9f325e5118a4b0dbb3d43d
Sjors Wijsman <sjorswijsman@gmail.com>
2023-02-20 23:18:46 +0100

    data: added 2.0 al mazrah spawns

--- src/data/al-mazrah/locations/spawns.js
+++ src/data/al-mazrah/locations/spawns.js
@@ -1,205 +1,336 @@
 export const spawns = [
 	{
-		title: "H7",
+		title: "B4",
 		location: {
-			x: 3030,
-			y: 2915,
+			x: 1785,
+			y: 664,
 		},
 	},
 	{
-		title: "G7",
+		title: "C5",
 		location: {
-			x: 2700,
-			y: 3200,
-		},
-	},
-	{
-		title: "F8",
-		location: {
-			x: 2132,
-			y: 3376,
-		},
-	},
-	{
-		title: "E8",
-		location: {
-			x: 1712,
-			y: 3415,
-		},
-	},
-	{
-		title: "D8",
-		location: {
-			x: 1456,
-			y: 3495,
-		},
-	},
-	{
-		title: "C7",
-		location: {
-			x: 1092,
-			y: 3088,
-		},
-	},
-	{
-		title: "C6",
-		location: {
-			x: 1061,
-			y: 2788,
-		},
-	},
-	{
-		title: "B5 (2)",
-		location: {
-			x: 691,
-			y: 2431,
-		},
-	},
-	{
-		title: "B3",
-		location: {
-			x: 716,
-			y: 1652,
+			x: 2382,
+			y: 841,
 		},
 	},
 	{
 		title: "C4",
 		location: {
-			x: 1186,
-			y: 1763,
+			x: 2018,
+			y: 1182,
 		},
 	},
 	{
-		title: "C3",
+		title: "D7",
 		location: {
-			x: 1124,
-			y: 1482,
+			x: 2941,
+			y: 1507,
 		},
 	},
 	{
-		title: "E4",
+		title: "E5",
 		location: {
-			x: 2046,
-			y: 1700,
+			x: 2440,
+			y: 1696,
 		},
 	},
 	{
-		title: "E2",
+		title: "E6",
 		location: {
-			x: 1966,
-			y: 1202,
+			x: 2790,
+			y: 1910,
 		},
 	},
 	{
-		title: "D4",
+		title: "E7",
 		location: {
-			x: 1581,
-			y: 1778,
+			x: 3151,
+			y: 2042,
 		},
 	},
 	{
-		title: "F3",
+		title: "F7",
 		location: {
-			x: 2315,
-			y: 1367,
+			x: 2989,
+			y: 2398,
 		},
 	},
 	{
-		title: "E1",
+		title: "G7",
 		location: {
-			x: 1952,
-			y: 754,
-		},
-	},
-	{
-		title: "D2",
-		location: {
-			x: 1444,
-			y: 1212,
-		},
-	},
-	{
-		title: "F2 (2)",
-		location: {
-			x: 2390,
-			y: 969,
-		},
-	},
-	{
-		title: "F2 (1)",
-		location: {
-			x: 2130,
-			y: 906,
-		},
-	},
-	{
-		title: "G2",
-		location: {
-			x: 2722,
-			y: 1148,
-		},
-	},
-	{
-		title: "H3",
-		location: {
-			x: 2930,
-			y: 1518,
-		},
-	},
-	{
-		title: "H4",
-		location: {
-			x: 2930,
-			y: 1793,
-		},
-	},
-	{
-		title: "G4",
-		location: {
-			x: 2718,
-			y: 1980,
-		},
-	},
-	{
-		title: "H5",
-		location: {
-			x: 3251,
-			y: 2221,
+			x: 2940,
+			y: 2778,
 		},
 	},
 	{
 		title: "H6",
 		location: {
-			x: 3048,
-			y: 2495,
+			x: 2731,
+			y: 3234,
 		},
 	},
 	{
-		title: "F4",
+		title: "H5",
 		location: {
-			x: 2222,
-			y: 1925,
+			x: 2310,
+			y: 2939,
 		},
 	},
 	{
-		title: "B5 (1)",
+		title: "I5",
 		location: {
-			x: 740,
-			y: 2081,
+			x: 2145,
+			y: 3385,
 		},
 	},
 	{
-		title: "B6",
+		title: "I3",
+		location: {
+			x: 1574,
+			y: 3454,
+		},
+	},
+	{
+		title: "H2",
+		location: {
+			x: 1057,
+			y: 3098,
+		},
+	},
+	{
+		title: "G3",
+		location: {
+			x: 1354,
+			y: 2758,
+		},
+	},
+	{
+		title: "G1",
 		location: {
 			x: 677,
 			y: 2705,
 		},
 	},
 	{
-		title: "D6",
+		title: "F2",
 		location: {
-			x: 1333,
-			y: 2765,
+			x: 880,
+			y: 2262,
+		},
+	},
+	{
+		title: "D3",
+		location: {
+			x: 1370,
+			y: 1617,
 		},
 	},
 ];
+
+// Updated after s2
+//
+// export const spawns = [
+// 	{
+// 		title: "H7",
+// 		location: {
+// 			x: 3030,
+// 			y: 2915,
+// 		},
+// 	},
+// 	{
+// 		title: "G7",
+// 		location: {
+// 			x: 2700,
+// 			y: 3200,
+// 		},
+// 	},
+// 	{
+// 		title: "F8",
+// 		location: {
+// 			x: 2132,
+// 			y: 3376,
+// 		},
+// 	},
+// 	{
+// 		title: "E8",
+// 		location: {
+// 			x: 1712,
+// 			y: 3415,
+// 		},
+// 	},
+// 	{
+// 		title: "D8",
+// 		location: {
+// 			x: 1456,
+// 			y: 3495,
+// 		},
+// 	},
+// 	{
+// 		title: "C7",
+// 		location: {
+// 			x: 1092,
+// 			y: 3088,
+// 		},
+// 	},
+// 	{
+// 		title: "C6",
+// 		location: {
+// 			x: 1061,
+// 			y: 2788,
+// 		},
+// 	},
+// 	{
+// 		title: "B5 (2)",
+// 		location: {
+// 			x: 691,
+// 			y: 2431,
+// 		},
+// 	},
+// 	{
+// 		title: "B3",
+// 		location: {
+// 			x: 716,
+// 			y: 1652,
+// 		},
+// 	},
+// 	{
+// 		title: "C4",
+// 		location: {
+// 			x: 1186,
+// 			y: 1763,
+// 		},
+// 	},
+// 	{
+// 		title: "C3",
+// 		location: {
+// 			x: 1124,
+// 			y: 1482,
+// 		},
+// 	},
+// 	{
+// 		title: "E4",
+// 		location: {
+// 			x: 2046,
+// 			y: 1700,
+// 		},
+// 	},
+// 	{
+// 		title: "E2",
+// 		location: {
+// 			x: 1966,
+// 			y: 1202,
+// 		},
+// 	},
+// 	{
+// 		title: "D4",
+// 		location: {
+// 			x: 1581,
+// 			y: 1778,
+// 		},
+// 	},
+// 	{
+// 		title: "F3",
+// 		location: {
+// 			x: 2315,
+// 			y: 1367,
+// 		},
+// 	},
+// 	{
+// 		title: "E1",
+// 		location: {
+// 			x: 1952,
+// 			y: 754,
+// 		},
+// 	},
+// 	{
+// 		title: "D2",
+// 		location: {
+// 			x: 1444,
+// 			y: 1212,
+// 		},
+// 	},
+// 	{
+// 		title: "F2 (2)",
+// 		location: {
+// 			x: 2390,
+// 			y: 969,
+// 		},
+// 	},
+// 	{
+// 		title: "F2 (1)",
+// 		location: {
+// 			x: 2130,
+// 			y: 906,
+// 		},
+// 	},
+// 	{
+// 		title: "G2",
+// 		location: {
+// 			x: 2722,
+// 			y: 1148,
+// 		},
+// 	},
+// 	{
+// 		title: "H3",
+// 		location: {
+// 			x: 2930,
+// 			y: 1518,
+// 		},
+// 	},
+// 	{
+// 		title: "H4",
+// 		location: {
+// 			x: 2930,
+// 			y: 1793,
+// 		},
+// 	},
+// 	{
+// 		title: "G4",
+// 		location: {
+// 			x: 2718,
+// 			y: 1980,
+// 		},
+// 	},
+// 	{
+// 		title: "H5",
+// 		location: {
+// 			x: 3251,
+// 			y: 2221,
+// 		},
+// 	},
+// 	{
+// 		title: "H6",
+// 		location: {
+// 			x: 3048,
+// 			y: 2495,
+// 		},
+// 	},
+// 	{
+// 		title: "F4",
+// 		location: {
+// 			x: 2222,
+// 			y: 1925,
+// 		},
+// 	},
+// 	{
+// 		title: "B5 (1)",
+// 		location: {
+// 			x: 740,
+// 			y: 2081,
+// 		},
+// 	},
+// 	{
+// 		title: "B6",
+// 		location: {
+// 			x: 677,
+// 			y: 2705,
+// 		},
+// 	},
+// 	{
+// 		title: "D6",
+// 		location: {
+// 			x: 1333,
+// 			y: 2765,
+// 		},
+// 	},
+// ];
